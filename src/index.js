efa1c86717ec684dfd3756aed28a8a4a168be65e
mohsiteboosthyd-coder <moh.siteboosthyd@gmail.com>
2025-10-27 14:47:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css'; // This imports your Tailwind CSS
-import App from '.App';
+import './style.css'; // You will create this file for Tailwind CSS
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  React.StrictMode
-    <App /> 
-  React.StrictMode
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-
-
